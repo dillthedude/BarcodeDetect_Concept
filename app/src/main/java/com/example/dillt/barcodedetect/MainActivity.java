24ff7130de6a1f50a5c5f2544c980f87e9e3b730
LiLDorth <LiLDorth@hotmail.com>
2017-11-06 14:43:27 -0700

    Barcode Lookup Code

--- app/src/main/java/com/example/dillt/barcodedetect/MainActivity.java
+++ app/src/main/java/com/example/dillt/barcodedetect/MainActivity.java
@@ -13,9 +13,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,5 +67,55 @@ public class MainActivity extends AppCompatActivity {
 
         txtView.setText(thisCode.rawValue);
         // <----- this app is going to be awesome!
+    }
+
+    public void barCodeRequest(View view) {
+        // https://api.upcdatabase.org/search/{id}/{api_key}
+        String cameraCode = "5705831011120"; // for testing
+        String OA = "B030EDF4EF23AB4A8D882639504629D6";
+        String url;// = "https://api.upcdatabase.org/search/" + cameraCode + "/" + OA;
+        url = "https://api.upcdatabase.org/search/5705831011120/B030EDF4EF23AB4A8D882639504629D6";
+
+        final TextView mTxtDisplay;
+        ImageView mImageView;
+        mTxtDisplay = (TextView) findViewById(R.id.mTxtDisplay);
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mTxtDisplay.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+
+        final TextView mTextView = (TextView) findViewById(R.id.textView);
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsObjRequest);
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
